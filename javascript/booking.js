45a56f428b152ce4a68f2ff657c89d4b5cdb5199
J Loo <junzhongloo@gmail.com>
2021-06-20 23:51:48 +1200

    clean js files

--- javascript/booking.js
+++ javascript/booking.js
@@ -3,24 +3,27 @@ window.addEventListener('DOMContentLoaded', () => {
     var seatCount = document.getElementById('seat-selected-count');
     var priceTotal = document.getElementById('total-price');
     const seats = document.getElementsByClassName('seat');
+    const ticketPrice = 12;
+
+    const setSeatDiv = () => {
+        seatCount.innerHTML = count;
+        priceTotal.innerHTML = `$${count * ticketPrice}.00`;
+        localStorage.setItem("tickets", count);
+    }
 
     /* select seat */
     Array.prototype.forEach.call(seats, (seat) => {
         seat.addEventListener('click', () => {
-            if (seat.classList.contains('seat-selected')){
+            if(seat.classList.contains('seat-selected')){
                 seat.classList.remove('seat-selected');
                 count -= 1;
-                seatCount.innerHTML = count;
-                priceTotal.innerHTML = `$${count * 12}.00`;
-                localStorage.setItem("tickets", count);
-            } else if (seat.classList.contains('seat-not-available')){
+                setSeatDiv();
+            } else if(seat.classList.contains('seat-not-available')){
                 alert('Seat not available');
             } else {
                 seat.classList.add('seat-selected');
                 count += 1;
-                seatCount.innerHTML = count;
-                priceTotal.innerHTML = `$${count * 12}.00`;
-                localStorage.setItem("tickets", count);
+                setSeatDiv();
             }
         })
     })
--- javascript/index.js
+++ javascript/index.js
@@ -1,15 +1,15 @@
 window.addEventListener('DOMContentLoaded', () => {
+
     /* Blur out of focus movie cards */
     const cards = document.getElementsByClassName('card')
-    Array.prototype.forEach.call(cards, element => {
-        element.addEventListener('mouseenter', () => {
+    Array.prototype.forEach.call(cards, card => {
+        card.addEventListener('mouseenter', () => {
             Array.prototype.forEach.call(cards, e => {
                 e.classList.add('inactive');
             })
-            element.classList.remove('inactive')
+            card.classList.remove('inactive')
         })
-
-        element.addEventListener('mouseleave', () => {
+        card.addEventListener('mouseleave', () => {
             Array.prototype.forEach.call(cards, e => {
                 e.classList.remove('inactive')
             })
@@ -17,18 +17,21 @@ window.addEventListener('DOMContentLoaded', () => {
     });
 
     /* main carousel change every 3 seconds at random */
-    const imageSlides = [
-        "banner.webp",
-        "imax.jpg",
-        "godzilla_large.jpg",
-        "venom_large.png",
-    ]
-    
-    const slideImageDiv = document.getElementById('slideImage')
-    function changeSlide(){
-        slideImageDiv.src = "../images/large/" + imageSlides[Math.floor(Math.random() * imageSlides.length)];
+    const changeSlideInterval = () => {
+        const imageSlides = [
+            "banner.webp",
+            "imax.jpg",
+            "godzilla_large.jpg",
+            "venom_large.png",
+        ]
+        
+        const slideImageDiv = document.getElementById('slideImage')
 
+        function changeSlide(){
+            slideImageDiv.src = "../images/large/" + imageSlides[Math.floor(Math.random() * imageSlides.length)];
+        }
+        setInterval(changeSlide, 3000);
     }
-
-    setInterval(changeSlide, 3000);
+    
+    changeSlideInterval();
 })
